81d48a2b66f732bdc54deeb2b5a710d9bdc3da50
ViniciusGabriel0 <vinic26077@gmail.com>
2022-02-25 20:19:39 -0300

    projeto tabuada

---
+++ script.js
@@ -0,0 +1,30 @@
+var valor = document.querySelector("div.container input")
+var tabuada = document.querySelector("div.container div.tabuada")
+const calcular = document.querySelector("div.container button")
+
+
+calcular.addEventListener("click",  () => {
+
+    tabuada.innerHTML = ""
+ 
+    if(Number(valor.value)< 1 ||Number (valor.valor) > 12){
+
+        
+
+        tabuada.innerHTML = "Insira um valor válido, de 1 a 12."
+
+        }else{
+
+            for(i = 1; i <=12; i ++){
+             var calc = String(i) + " x " + valor.value + " = " + i * Number(valor.value);
+        
+
+        var resultado = document.createElement("p")
+
+        resultado.innerText = calc
+
+        tabuada.appendChild(resultado)
+            }
+        }
+
+})
